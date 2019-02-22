d1b6f2bbf035db2e82118f2fc03c55356f39e383
jiaweniscute <ng.jia.wen@hotmail.com>
2019-02-22 08:50:58 -0500

    added uploaddata functin

--- studentServer.js
+++ studentServer.js
@@ -9,8 +9,9 @@ var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
 
-app.get('/',function (req,res) {
-res.send("hello world from the HTTP server"); });
+app.get('/', function (req, res) {
+    res.send("hello world from the HTTP server");
+});
 
 // process uploaded data
 var bodyParser = require('body-parser');
@@ -43,7 +44,6 @@ app.use(function (req, res, next) {
 });
 
 
-
 // test out database connection
 app.get('/postgistest', function (req, res) {
     pool.connect(function (err, client, done) {
@@ -62,19 +62,45 @@ app.get('/postgistest', function (req, res) {
     });
 });
 
-app.post('/reflectData',function(req,res){
-       // note that we are using POST here as we are uploading data
-        // so the parameters form part of the BODY of the request rather
-      //than the RESTful API
-       console.dir(req.body);
-       // for now, just echo the request back to the client
-       res.send(req.body);
+app.post('/reflectData', function (req, res) {
+    // note that we are using POST here as we are uploading data
+    // so the parameters form part of the BODY of the request rather
+    //than the RESTful API
+    console.dir(req.body);
+    // for now, just echo the request back to the client
+    res.send(req.body);
 });
 
+app.post('/uploadData', function (req, res) {
+    // note that we are using POST here as we are uploading data
+    // so the parameters form part of the BODY of the request rather than the RESTful API
+    console.dir(req.body);
+    pool.connect(function (err, client, done) {
+        if (err) {
+            console.log("not able to get connection " + err);
+            res.status(400).send(err);
+        }
+        var name = req.body.name;
+        var surname = req.body.surname;
+        var module = req.body.module;
+        var portnum = req.body.port_id;
+        var language = req.body.language;
+        var modulelist = req.body.modulelist;
+        var lecturetime = req.body.lecturetime;
+        var geometrystring = "st_geomfromtext('POINT(" + req.body.longitude + " " + req.body.latitude + ")')";
 
-
-
-
-
+        var querystring = "INSERT into formdata (name,surname,module, port_id,language, modulelist, lecturetime, geom) values ($1,$2,$3,$4,$5,$6,$7,";
+        var querystring = querystring + geometrystring + ") ";
+        console.log(querystring);
+        client.query(querystring, [name, surname, module, portnum, language, modulelist, lecturetime], function (err, result) {
+            done();
+            if (err) {
+                console.log(err);
+                res.status(400).send(err);
+            }
+            res.status(200).send("row inserted");
+        });
+    });
+});
 
 app.use(express.static(__dirname));
